b17db8bdc0d9ceb3e9fa31f4316907707817d2d3
Alexey Melezhik <melezhik@gmail.com>
2016-11-15 13:44:12 +0300

    update default package list

--- modules/pkg/install/story.bash
+++ modules/pkg/install/story.bash
@@ -2,5 +2,5 @@ pkg=$(story_var 'pkg')
 version=$(story_var 'version')
 
 echo install $pkg - $version
-
+echo "gonna do this: opkg -f /etc/opkg/opkg.conf install $pkg"
 opkg -f /etc/opkg/opkg.conf install $pkg
