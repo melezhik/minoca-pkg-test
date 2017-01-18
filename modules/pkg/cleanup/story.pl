22da89bf7fbff62103ee27cbc35cca6b436cfca9
Alexey Melezhik <melezhik@gmail.com>
2017-01-18 17:44:50 +0300

    basic-package

--- modules/pkg/cleanup/story.pl
+++ modules/pkg/cleanup/story.pl
@@ -7,16 +7,18 @@ open( my $fh, test_root_dir().'/installed.txt') or die "can't open file with ins
 while( my $l = <$fh>) {
   chomp $l;
   $installed{$l}=1;
-  print "basic package: $l ... \n";	
 }
 
 open(my $fh, '-|', 'opkg list-installed') or die "can't open `opkg list-installed` for read: $!";
 my $status = "ok";
 while ( my $p = <$fh>){
   chomp $p;
-  next if $installed{$p};
-  print "removing $p ... \n";	
-  $status = "failed" unless system("opkg --force-depends $p") == 0;
+  if $installed{$p} {
+    print "skip $p removal as it basic package\n";	
+  } else {
+    print "removing $p ... \n";	
+    $status = "failed" unless system("opkg --force-depends $p") == 0;
+  }
   
 }
 
