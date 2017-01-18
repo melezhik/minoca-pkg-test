18d4bd184e2e18c5cc1f0331f34a885bf4967e63
Alexey Melezhik <melezhik@gmail.com>
2017-01-18 17:41:23 +0300

    first version

--- modules/pkg/cleanup/story.pl
+++ modules/pkg/cleanup/story.pl
@@ -1,13 +1,25 @@
 use strict;
 
-open(my $fh, '-|', 'opkg list-installed') or die "can't open `opkg list-installed` for read: $!";
-while ( my $l = <$fh>){
-  print $l;	
-}
-close $fh;
+my %installed;
 
 open( my $fh, test_root_dir().'/installed.txt') or die "can't open file with installed packages list to read: $!";
 
-while( my $l = ){
-
+while( my $l = <$fh>) {
+  chomp $l;
+  $installed{$l}=1;
 }
+
+open(my $fh, '-|', 'opkg list-installed') or die "can't open `opkg list-installed` for read: $!";
+my $status = "ok";
+while ( my $p = <$fh>){
+  chomp $p;
+  next if $installed{$p};
+  print "removing $p ... \n";	
+  $status = "failed" unless system("opkg --force-depends $p") == 0;
+  
+}
+
+close $fh;
+
+print "status: $status";
+
